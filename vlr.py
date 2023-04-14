3af4c16496a90e78a0432af5c0b1d2a6fdb77e91
1khanh <khanh2@hawaii.edu>
2023-04-13 16:15:20 -1000

    Update: Successful Player Stats Grabbing

--- vlr.py
+++ vlr.py
@@ -14,7 +14,8 @@ team_div = soup.find('div', class_='mod-scroll')
 team_items = team_div.find_all('div', class_='rank-item')
 
 data = []
-for i, team in enumerate(team_items[:10]):
+stats = []
+for i, team in enumerate(team_items[:1]):
     team_rank = team.find('div', class_='rank-item-rank-num').get_text(strip=True)
     team_name_elem = team.find('div', class_='ge-text')
 
@@ -34,7 +35,7 @@ for i, team in enumerate(team_items[:10]):
     managers = []
     coaches = []
     players = []
-    team_roster = team_soup.find_all('div', class_='team-roster-item-name')
+    team_roster = team_soup.find_all('div', class_='team-roster-item')
 
     for player in team_roster:
         alias = player.find('div', class_='team-roster-item-name-alias')
@@ -59,8 +60,72 @@ for i, team in enumerate(team_items[:10]):
         else:
             players.append(player_name)
 
-    data.append([team_rank, team_name, team_igl, players, managers, coaches, href])
+        player_href = player.find('a').get('href')
+        player_url = 'https://www.vlr.gg' + player_href
+        player_url60 = player_url + '/?timespan=60d'
+        # print(player_url60)
 
-df = pd.DataFrame(data, columns=['Rank', 'Team', 'Team Leader', 'Players', 'Manager', 'Coach(es)','Link'])
+        player_response = requests.get(player_url60)
+        player_soup = BeautifulSoup(player_response.content, 'html.parser')
+        table = player_soup.find('table')
+        # print(table)
+        
+        player_stats = []
+        rows = table.find_all('tr')[1:]  # Skip the header row
 
-print(df)
+        for row in rows:
+            cells = row.find_all('td')
+            agent = cells[0].find('img')['alt']
+            usage = cells[1].text.strip()
+            rounds_played = cells[2].text.strip()
+            rating = cells[3].text.strip()
+            avg_combat_score = cells[4].text.strip()
+            kd_ratio = cells[5].text.strip()
+            avg_damage_per_round = cells[6].text.strip()
+            kast = cells[7].text.strip()
+            kpr = cells[8].text.strip()
+            apr = cells[9].text.strip()
+            fkpr = cells[10].text.strip()
+            fdpr = cells[11].text.strip()
+            kills = cells[12].text.strip()
+            deaths = cells[13].text.strip()
+            assists = cells[14].text.strip()
+            first_bloods = cells[15].text.strip()
+            first_deaths = cells[16].text.strip()
+
+            # Create a dictionary to store the extracted data
+            row_data = {
+                'Alias': player_name,
+                'Agent': agent,
+                'Usage': usage,
+                'Rounds Played': rounds_played,
+                'Rating': rating,
+                'Average Combat Score': avg_combat_score,
+                'Kills:Death': kd_ratio,
+                'Average Damage per Round': avg_damage_per_round,
+                'Kill, Assist, Survive, Trade %': kast,
+                'Kills Per Round': kpr,
+                'Assists Per Round': apr,
+                'First Kills Per Round': fkpr,
+                'First Deaths Per Round': fdpr,
+                'Kills': kills,
+                'Deaths': deaths,
+                'Assists': assists,
+                'First Bloods': first_bloods,
+                'First Deaths': first_deaths
+            }
+
+            # Add the row data to the list
+            player_stats.append(row_data)
+
+        # Print the extracted data
+        for row_data in player_stats:
+            print(row_data)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+#     data.append([team_rank, team_name, team_igl, players, managers, coaches, href])
+    
+
+# df = pd.DataFrame(data, columns=['Rank', 'Team', 'Team Leader', 'Players', 'Manager', 'Coach(es)','Link'])
+
+# print(df)
